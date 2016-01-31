47dc5593e7cf5574590cc546c4ac4ec4fa8d4bfd
Jake Graham <tehgrahamcracka@gmail.com>
2016-01-31 00:06:06 -0600

    Game
    Added game to repo.

---
+++ Pong/Assets/Scripts/BallScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+
+	[SerializeField]
+	float forceValue = 4.5f;
+	Rigidbody2D myBody;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(Reset());
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator Reset() 
+	{
+		myBody = GetComponent<Rigidbody2D>();
+		yield return WaitForSeconds (3);
+		// reset the ball position and restart the ball movement
+		myBody.velocity = Vector2.zero;
+		transform.position = new Vector2(0,0);
+		myBody.AddForce (new Vector2 (forceValue * 50, 50));
+		forceValue = forceValue * -1;
+	}
+
+	public void Stop()
+	{
+		// this method stops the ball
+		myBody.velocity = Vector2.zero;
+	}
+}
---
+++ ReadMe.md
@@ -0,0 +1,2 @@
+# Pong-Clone
+I followed this [tutorial](http://unity.grogansoft.com/beginners-guide-create-a-pong-clone-in-unity-part-1/) to make this. 
